522a0ed9b3d719de64662dd89b07763a98e9e86d
Solange Duhimbaze Ihirwe <159579750+solangeihirwe03@users.noreply.github.com>
2024-09-05 19:14:10 +0200

    set up project environment

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, {Response, Request} from "express";
+import cors from "cors"
+import httpStatus from "http-status";
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express();
+
+app.use(cors());
+app.get("/", (req: Request, res: Response)=> {
+    res.status(httpStatus.OK).json({
+        status: httpStatus.OK,
+        message: "Welcome to our shopsmart backend"
+    })
+})
+
+const port = process.env.PORT || 9090
+
+app.listen(port, ()=> console.log(`Server is running on ${port}`))
